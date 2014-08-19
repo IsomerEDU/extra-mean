1a16db6323fa643dbdceb0c8d49e6eccfbe04ac3
Max Johansen <macsj200@gmail.com>
2014-08-19 14:04:14 -0600

    update newcert.sh

--- config/certs/newcert.sh
+++ config/certs/newcert.sh
@@ -1,2 +1,18 @@
 #!/bin/sh
 
+#script to create a new self-signed certificate
+#generates cert files good for 365 days
+
+#based on http://www.akadia.com/services/ssh_test_certificate.html
+#<a href="http://www.akadia.com/services/ssh_test_certificate.html"></a>
+
+#run this in the directory that you want the certs created in
+
+# server.crt is the certificate
+# server.key is the key
+
+openssl genrsa -des3 -out server.key 1024
+openssl req -new -key server.key -out server.csr
+cp server.key server.key.org
+openssl rsa -in server.key.org -out server.key
+openssl x509 -req -days 365 -in server.csr -signkey server.key -out server.crt
